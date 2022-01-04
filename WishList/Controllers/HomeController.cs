f133b500dadf4ec0028e69d74079cb803a0c0426
anarraidoo <32473241+anarraidoo@users.noreply.github.com>
2022-01-04 15:40:04 -0500

    Step 3

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
